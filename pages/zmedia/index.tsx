94c7164d5054cf7fe54d9979d6a7325456df2a3c
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2023-08-13 19:53:47 -0400

    create a folder path route

---
+++ pages/zmedia/index.tsx
@@ -0,0 +1,5 @@
+//domian.com/zmedia
+
+export default function MediaHomepage() {
+  return <h1>This is MediaHomepage</h1>;
+}
---
+++ pages/zmedia/zmediadetails.tsx
@@ -0,0 +1,5 @@
+//domian.com/zmedia/zmediadetails casesensitive
+
+export default function ZMediaDetailsHomepage() {
+  return <h1>This is zmediadetailsHomepage</h1>;
+}

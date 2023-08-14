0c6bc9803d9e81a78a00f45d4bdcd85dcbbee6d5
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2023-08-13 21:07:35 -0400

    update UI and add a link to subpage

--- pages/zmedia/index.tsx
+++ pages/zmedia/index.tsx
@@ -1,5 +1,55 @@
 //domian.com/zmedia
 
+import { Card, CardHeader, CardBody } from "@nextui-org/react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@nextui-org/react";
+import NextLink from "next/link";
+
 export default function MediaHomepage() {
-  return <h1>This is MediaHomepage</h1>;
+  return (
+    <Card>
+      <CardHeader>This is MediaHomepage</CardHeader>
+      <CardBody>
+        <Table isStriped aria-label="Media collection table">
+          <TableHeader>
+            <TableColumn>NAME</TableColumn>
+            <TableColumn>TIME</TableColumn>
+            <TableColumn>STATUS</TableColumn>
+          </TableHeader>
+          <TableBody>
+            <TableRow key="1">
+              <TableCell>
+                <a href="https://nextjs.org/docs">NEXTJS learning</a>
+              </TableCell>
+              <TableCell>1 day ago</TableCell>
+              <TableCell>Active</TableCell>
+            </TableRow>
+            <TableRow key="2">
+              <TableCell>NEXTUI learning</TableCell>
+              <TableCell>19 hr ago</TableCell>
+              <TableCell>Active</TableCell>
+            </TableRow>
+            <TableRow key="3">
+              <TableCell>
+                <NextLink href="/zmedia/zmediadetails">zmediadetails</NextLink>
+              </TableCell>
+              <TableCell>3 hr ago</TableCell>
+              <TableCell>Active</TableCell>
+            </TableRow>
+            <TableRow key="4">
+              <TableCell>Wildfires</TableCell>
+              <TableCell>4 hr ago</TableCell>
+              <TableCell>Active</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardBody>
+    </Card>
+  );
 }

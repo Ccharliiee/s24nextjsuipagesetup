98b068aaea3acfb27ef883e8a8f2d1fab62d6f20
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2023-08-14 21:41:17 -0400

    create a new indexpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,24 @@
-import NextLink from "next/link";
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-import DefaultLayout from "@/layouts/default";
+import MeetupList from "@/components/meetups/MeetupList";
+
+const sistaticevents = [
+  {
+    id: "ik1",
+    title: "A First staticevent",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "5 street, One City",
+    description: "This is a first staticevent!",
+  },
+  {
+    id: "ik2",
+    title: "A Second staticevent",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b6/Mount_Everest_as_seen_from_Drukair2_PLW_edit_Cropped.jpg",
+    address: "10 ave, Tea City",
+    description: "This is a second staticevent!",
+  },
+];
 
 export default function IndexPage() {
-	return (
-		<DefaultLayout>
-			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-				<div className="inline-block max-w-lg text-center justify-center">
-					<h1 className={title()}>Make&nbsp;</h1>
-					<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-					<br />
-					<h1 className={title()}>
-						websites regardless of your design experience.
-					</h1>
-					<h4 className={subtitle({ class: "mt-4" })}>
-						Beautiful, fast and modern React UI library.
-					</h4>
-				</div>
-
-				<div className="flex gap-3">
-					<Link
-						isExternal
-						as={NextLink}
-						href={siteConfig.links.docs}
-						className={buttonStyles({
-							color: "primary",
-							radius: "full",
-							variant: "shadow",
-						})}
-					>
-						Documentation
-					</Link>
-					<Link
-						isExternal
-						as={NextLink}
-						className={buttonStyles({ variant: "bordered", radius: "full" })}
-						href={siteConfig.links.github}
-					>
-						<GithubIcon size={20} />
-						GitHub
-					</Link>
-				</div>
-
-				<div className="mt-8">
-					<Snippet hideSymbol hideCopyButton variant="bordered">
-						<span>
-							Get started by editing <Code color="primary">pages/index.tsx</Code>
-						</span>
-					</Snippet>
-				</div>
-			</section>
-		</DefaultLayout>
-	);
+  return <MeetupList meetups={sistaticevents} />;
 }

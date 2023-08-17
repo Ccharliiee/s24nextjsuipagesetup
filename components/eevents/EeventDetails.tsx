ce8d920bef246b301ab3c13d23725abceef658fb
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2023-08-17 19:41:17 -0400

    Use Image component in "@nextui-org/react"

--- components/eevents/EeventDetails.tsx
+++ components/eevents/EeventDetails.tsx
@@ -1,9 +1,10 @@
+import { Image } from "@nextui-org/react";
 import classes from "./EeventDetails.module.css";
 
 const EeventDetail = (props: any) => {
   return (
     <section className={classes.detail}>
-      <img src={props.image} alt={props.title} />
+      <Image src={props.image} alt={props.title} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
